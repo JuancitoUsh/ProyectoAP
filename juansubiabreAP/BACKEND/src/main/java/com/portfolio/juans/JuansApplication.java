b0d5255f60322dba68d99637af73fe9d069ad8c2
JuancitoUsh <99854718+JuancitoUsh@users.noreply.github.com>
2022-07-24 15:58:44 -0300

    Creación de Carpeta FRONTEND
    Se creo la carpeta FRONTEND y se coloco la info adentro

---
+++ juansubiabreAP/BACKEND/src/main/java/com/portfolio/juans/JuansApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.juans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuansApplication.class, args);
+	}
+
+}
